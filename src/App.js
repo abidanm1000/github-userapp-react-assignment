f82fd3f35ad63759b5bb43367226d8efca6fa2a1
Abidan Martinez <abidanmtz@gmail.com>
2022-03-19 00:40:02 -0700

    adding API changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-
+import axios from 'axios'
+import { useEffect, useState, useRef } from 'react'
 
 // #1 npm install axios
 // #2 import axios from './axios'
@@ -13,14 +14,45 @@ import './App.css';
 // #10 Turn reusable sections of the app to components (Container,UserContent,UserInput.Finish rest of hw)
 
 function App() {
+  
+  // console.log(response.data)
 
+  const [userData, setUserData] = useState({})
+  const [user, setUser] = useState('octocat')
+  const [theme, setTheme] = useState('')
 
+  useEffect(()=> {
+    const getUser = async () => {
+      try {
+        const response = await axios.get(`https://api.github.com/users/${user}`)
+        .then((res)=>{
+          setUserData(res.data)
+        })
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+    
+    getUser()
+  }, [user])
+  console.log(userData)
+  const userRef = useRef()
 
+  // formatting the date
+  const date = new Date(userData.created_at)
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-
+  const changeTheme = () => {
+    if(theme == '') {
+      setTheme('dark')
+    } else {
+      setTheme('')
+    }
+  }
 
   return (
-      <div className="App">
+      <div className={`App ${theme}`}>
         <div className='container'>
 
 
@@ -28,15 +60,16 @@ function App() {
             <h4>devfinder</h4>
             <div className="darkMode" >
               <p id="modeText">DARK</p>
-              <img src="./assets/icon-moon.svg" id = "dark_btn"alt=""/>
+              <img src="./assets/icon-moon.svg" id = "dark_btn"alt="" onClick={changeTheme}/>
             </div>        
           </div>
 
           <div className="search" id="search_container">
             <img src="./assets/icon-search.svg" alt=""/>
-            <input id ="input" type="text" placeholder="Search Github username..."/>
+            <input ref={userRef} id ="input" type="text" placeholder="Search Github username..."/>
             <div className="search_btn">
-              <button id="search">Search</button>
+              <button id="search" onClick={()=> setUser(userRef.current.value)}
+              >Search</button>
             </div>
           </div>
 
@@ -48,7 +81,7 @@ function App() {
     
             <div className="profile">
               <div className="profile_frame">
-                <img id = "avatar"src="./assets/profilePlaceholder.png" alt=""/>
+                <img id = "avatar"src={!userData.avatar_url ? "./assets/profilePlaceholder.png" : userData.avatar_url} alt=""/>
               </div>
             </div>
 
@@ -58,20 +91,20 @@ function App() {
 
                 <div className="name" >
 
-                  <h3 id = "name">The Octocat</h3>
+                  <h3 id = "name">{userData.name}</h3>
 
                   <div className="username">
-                    <p id="login">@octocat</p>
+                    <p id="login">@{userData.login}</p>
                   </div>
                 </div>
           
                 <div className="date">
-                  <p id="date">Joined 25 Jan 2011</p>
+                  <p id="date">Joined {date.getDate()} {months[date.getMonth()]} {date.getFullYear()}</p>
                 </div>
               </div>
     
               <div className="bio">
-                <p id = "bio">This profile has no bio</p>
+                <p id = "bio">{!userData.bio ? 'This profile has no bio' : userData.bio}</p>
               </div>
 
 
@@ -79,17 +112,17 @@ function App() {
 
                 <div className="stats">
                   <p>Repos</p>
-                  <p id="repo">8</p>
+                  <p id="repo">{userData.public_repos}</p>
                 </div>
 
                 <div className="stats">
                   <p>Followers</p>
-                  <p id="followers">4443</p>
+                  <p id="followers">{userData.followers}</p>
                 </div>
 
                 <div className="stats">
                   <p>Following</p>
-                  <p id="following">9</p>
+                  <p id="following">{userData.following}</p>
                 </div>
 
               </div>
@@ -99,36 +132,28 @@ function App() {
                 <div className="left">
                   <div className="info">
                     <img src="./assets/icon-location.svg" alt=""/>
-                    <p id="location">San Francisco</p>
+                    <p id="location">{!userData.location ? 'Unavailable' : userData.location}</p>
                   </div>
                   <div className="info">
                     <img src="./assets/icon-website.svg" alt=""/>
-                    <a href="link" id = "blog">https://github.blog</a>
+                    <a href={userData.html_url} target="_blank" id = "blog">{userData.html_url}</a>
                   </div>
                 </div>
 
                 <div className="right">
                   <div className="info">
                     <img src="./assets/icon-twitter.svg" alt=""/>
-                    <p id ="twitter">Not Available</p>
+                    <p id ="twitter">{!userData.twitter_username ? 'Unavailable' : userData.twitter_username}</p>
                   </div>
                   <div className="info">
                     <img src="./assets/icon-company.svg" alt=""/>
-                  <p id= "company">@github</p>
+                  <p id= "company">{!userData.company ? 'Unavailable' : userData.company}</p>
                   </div>
                 </div>
 
               </div>
             </div>
-
-
-    
-
-    
-    
-    
           </div>
-
        </div>
      </div>
 
